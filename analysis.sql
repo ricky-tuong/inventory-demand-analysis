c7441fb3e1247ebf9f97ce67fa5ca0f192d7394d
ricky-tuong <rtuong2@gmail.com>
2026-01-07 16:59:09 -0500

    Add files via upload

---
+++ analysis.sql
@@ -0,0 +1,8 @@
+SELECT * FROM total_demand_per_product
+ORDER BY total_units_sold DESC;
+
+SELECT * FROM reorder_alerts
+ORDER BY inventory_status DESC;
+
+SELECT * FROM daily_demand_trend
+ORDER BY order_date;
